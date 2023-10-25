535e8c26da16d55821388b67efa8a63630bba0e3
doublebyte <doublebyte@hushmail.com>
2023-10-25 20:27:07 +0100

    - updated configuration for korea

--- docker/examples/elastic/ES/add_data.sh
+++ docker/examples/elastic/ES/add_data.sh
@@ -43,20 +43,11 @@ until $(curl -sSf -XGET --insecure 'http://localhost:9200/_cluster/health?wait_f
     sleep 10
 done
 
-echo "Elasticsearch seems to be working - Adding ArqMadeira_Troco_CAOP2022.geojson to ES"
-python3 /load_es_data.py /usr/share/elasticsearch/data/ArqMadeira_Troco_CAOP2022.geojson fid
-
-echo "Elasticsearch seems to be working - Adding ArqMadeira_NUTSI_CAOP2022.geojson to ES"
-python3 /load_es_data.py /usr/share/elasticsearch/data/ArqMadeira_NUTSI_CAOP2022.geojson fid
-
-echo "Elasticsearch seems to be working - Adding EF_AguasBalneares.geojson to ES"
-python3 /load_es_data.py /usr/share/elasticsearch/data/EF_AguasBalneares.geojson id
-
 echo "Elasticsearch seems to be working - Adding ne_110m_populated_places_simple.geojson to ES"
 python3 /load_es_data.py /ne_110m_populated_places_simple.geojson geonameid
 
-echo "Elasticsearch seems to be working - Adding pop_residente_2011_1k.geojson to ES"
-python3 /load_es_data.py /usr/share/elasticsearch/data/pop_residente_2011_1k.geojson objectid
+echo "Elasticsearch seems to be working - Adding roads_korea.geojson to ES"
+python3 /load_es_data.py /usr/share/elasticsearch/data/roads_korea.geojson osm_id
 
 echo "Seems that data was loaded"
 
